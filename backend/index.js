0edb560a0ae7efdca4aa918afe4b1e525cf145b1
Subha-5 <subhasadhu5@gmail.com>
2022-11-26 02:03:58 +0530

    created file structure for the application

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
+const express = require('express')
 const connectToMongo = require('./db');
 
 connectToMongo()
 
-
-const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.get('/', (req, res) =>
   res.send('Hello Coder!')
-})
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
