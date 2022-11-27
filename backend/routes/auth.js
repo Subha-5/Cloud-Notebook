2d9b2af00c110909a4f02955bbddf668999fbda2
Subha-5 <subhasadhu5@gmail.com>
2022-11-28 01:41:35 +0530

    Adding security layers

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const User = require('../models/User')
-
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'coderisagood$oy'
 
 // Create a user using: POST "/api/auth/createuser". No login required 
 router.post('/createuser',
@@ -24,11 +28,15 @@ router.post('/createuser',
             if (user) {
                 return res.status(400).json({ error: "Sorry a user with this email already exists." })
             }
+            const salt = await bcrypt.genSalt(10)
+            const secPass = await bcrypt.hash(req.body.password, salt)
+
             // create a new user
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                // password: req.body.password
+                password: secPass
             })
             /*.then(user => res.json(user))
                 .catch(err => {
@@ -40,7 +48,15 @@ router.post('/createuser',
                 })*/
 
             // res.send(req.body)
-            res.json(user) // response
+            // res.json(user) // response ( ⚠️ sending all details back) 
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET)
+            // console.log(authToken)
+            res.json({authToken})
         }
         // catching error if something unexpected occurs
         catch (error) {
