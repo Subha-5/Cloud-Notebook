761f88ed3972073da4aae22b487934a73a5eb616
Subha-5 <subhasadhu5@gmail.com>
2022-11-29 03:28:47 +0530

    Created all the basic CRUD endpoints for notes and the basic authentication

--- backend/middleware/fetchUser.js
+++ backend/middleware/fetchUser.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
-const JWT_SECRET = 'coderisagood$oy'
+// const secret = process.env.JWT_SECRET
+const secret = 'coderisagood$oy'
 
 const fetchUser = (req, res, next) => {
     // Get the user from the jwt token and add id to req object
@@ -9,7 +10,7 @@ const fetchUser = (req, res, next) => {
         res.status(401).send({ error: "Please authenticate using a valid token" })
     }
     try {
-        const data = jwt.verify(token, JWT_SECRET)
+        const data = jwt.verify(token, secret)
         req.user = data.user;
         next()
     } catch (error) {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,63 +8,66 @@ var jwt = require('jsonwebtoken');
 
 const fetchUser = require('../middleware/fetchUser')
 
-const JWT_SECRET = 'coderisagood$oy'
+// const secret = process.env.JWT_SECRET
+const secret = 'coderisagood$oy'
 
 // Route 1: Create a user using: POST "/api/auth/createuser". No login required 
-router.post('/createuser', [
-    body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 characters.').isLength({ min: 5 })
-], async (req, res) => {
-    // If there are errors, return Bad request and the errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    /* const user = User(req.body)
-    user.save() */
-    // Check whether the user with this email exists already
-    try {
-        let user = await User.findOne({ email: req.body.email })
-        if (user) {
-            return res.status(400).json({ error: "Sorry a user with this email already exists." })
+router.post('/createuser',
+    [
+        body('name', 'Enter a valid name').isLength({ min: 3 }),
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Password must be atleast 5 characters.').isLength({ min: 5 })
+    ],
+    async (req, res) => {
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-        const salt = await bcrypt.genSalt(10)
-        const secPass = await bcrypt.hash(req.body.password, salt)
-
-        // create a new user
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            // password: req.body.password
-            password: secPass
-        })
-        /*.then(user => res.json(user))
-            .catch(err => {
-                console.log(err)
-                res.json({
-                    error: "Please enter a unique value for email.",
-                    message: err.message
-                })
-            })*/
-
-        // res.send(req.body)
-        // res.json(user) // response ( ⚠️ sending all details back) 
-        const data = {
-            user: {
-                id: user.id
+        /* const user = User(req.body)
+        user.save() */
+        // Check whether the user with this email exists already
+        try {
+            let user = await User.findOne({ email: req.body.email })
+            if (user) {
+                return res.status(400).json({ error: "Sorry a user with this email already exists." })
             }
+            const salt = await bcrypt.genSalt(10)
+            const secPass = await bcrypt.hash(req.body.password, salt)
+
+            // create a new user
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                // password: req.body.password
+                password: secPass
+            })
+            /*.then(user => res.json(user))
+                .catch(err => {
+                    console.log(err)
+                    res.json({
+                        error: "Please enter a unique value for email.",
+                        message: err.message
+                    })
+                })*/
+
+            // res.send(req.body)
+            // res.json(user) // response ( ⚠️ sending all details back) 
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, secret)
+            // console.log(authToken)
+            res.json({ authToken: authToken })
         }
-        const authToken = jwt.sign(data, JWT_SECRET)
-        // console.log(authToken)
-        res.json({ authToken: authToken })
-    }
-    // catching error if something unexpected occurs
-    catch (error) {
-        console.error(error.message)
-        res.status(500).send("Internal Server error")
-    }
-})
+        // catching error if something unexpected occurs
+        catch (error) {
+            console.error(error.message)
+            res.status(500).send("Internal Server error")
+        }
+    })
 
 // Route 2: Authenticate a user using: POST "/api/auth/login". No login required 
 router.post('/login', [
@@ -96,7 +99,7 @@ router.post('/login', [
                 id: user.id
             }
         }
-        const authToken = jwt.sign(data, JWT_SECRET)
+        const authToken = jwt.sign(data, secret)
         res.json({ authToken })
 
     } catch (error) {
